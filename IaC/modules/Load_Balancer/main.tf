6f5efa77bfe66cd873ea8ad9a0f45180dfbce0b0
evangelostsak <baggtsak55@gmail.com>
2025-03-10 22:45:56 +0100

    terraform alb

---
+++ IaC/modules/Load_Balancer/main.tf
@@ -0,0 +1,35 @@
+resource "aws_lb" "app_lb" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_security_group_id]
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = var.target_group_name
+  port     = var.target_group_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = var.health_check_path
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
