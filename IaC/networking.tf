367ebc9c7ed1241eb738ab5c10631fc4ea013133
evangelostsak <baggtsak55@gmail.com>
2025-07-19 23:59:16 +0200

    custom vpc and subnets updated

--- IaC/networking.tf
+++ IaC/networking.tf
@@ -1,17 +1,89 @@
-data "aws_vpc" "default_vpc" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.app_name}-${var.environment_name}-vpc"
   }
 }
 
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+  tags = { Name = "public-a" }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+  tags = { Name = "public-b" }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "eu-central-1c"
+  map_public_ip_on_launch = true
+  tags = { Name = "public-c" }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "eu-central-1a"
+  tags = { Name = "private-a" }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.102.0/24"
+  availability_zone = "eu-central-1b"
+  tags = { Name = "private-b" }
+}
+
+resource "aws_subnet" "private_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.103.0/24"
+  availability_zone = "eu-central-1c"
+  tags = { Name = "private-c" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 resource "aws_security_group" "ec2_sg" {
   name = "${var.app_name}-${var.environment_name}-instance-security-group"
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id = aws_vpc.main.id
 }
 
 # Allow ALB to send traffic to Flask on EC2 (Port 5000)
@@ -60,7 +132,7 @@ resource "aws_security_group_rule" "allow_ec2_all_outbound" {
 
 resource "aws_security_group" "alb" {
   name   = "${var.app_name}-${var.environment_name}-alb-security-group"
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id = aws_vpc.main.id
 }
 
 # Allow public access to ALB on Port 80
@@ -87,7 +159,7 @@ resource "aws_security_group_rule" "allow_alb_flask_outbound" {
 
 resource "aws_security_group" "rds_sg" {
   name   = "${var.app_name}-${var.environment_name}-rds-security-group"
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id = aws_vpc.main.id
 }
 
 # Allow EC2 to access PostgreSQL on RDS
@@ -115,7 +187,7 @@ resource "aws_security_group_rule" "allow_rds_all_outbound" {
 resource "aws_lb" "load_balancer" {
   name               = "${var.app_name}-${var.environment_name}-web-app-lb"
   load_balancer_type = "application"
-  subnets = data.aws_subnets.default.ids
+  subnets = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.public_c.id]
   security_groups    = [aws_security_group.alb.id]
 }
 
@@ -134,7 +206,7 @@ resource "aws_lb_target_group" "instances" {
   name     = "${var.app_name}-${var.environment_name}-tg"
   port     = 5000
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default_vpc.id
+  vpc_id   = aws_vpc.main.id
 
   health_check {
     path                = "/"
@@ -145,10 +217,4 @@ resource "aws_lb_target_group" "instances" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
-}
-
-resource "aws_lb_target_group_attachment" "instance_1" {
-  target_group_arn = aws_lb_target_group.instances.arn
-  target_id        = aws_instance.instance_1.id
-  port             = 5000
 }
