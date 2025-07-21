adf9beb2babb562b912e5215fbe2dc776cc8e346
evangelostsak <baggtsak55@gmail.com>
2025-07-21 22:36:54 +0200

    variables updated for vpc, subnets, asg and cloudwatch

--- IaC/variables.tf
+++ IaC/variables.tf
@@ -40,7 +40,52 @@ variable "key_name" {
 variable "allowed_ssh_ip" {
   description = "IP address to allow SSH access"
   type        = string
-  
+}
+
+# ASG Variables
+
+variable "asg_min_size" {
+  description = "Minimum number of instances in Auto Scaling Group"
+  type        = number
+  default     = 3
+}
+
+variable "asg_max_size" {
+  description = "Maximum number of instances in Auto Scaling Group"
+  type        = number
+  default     = 3
+}
+
+variable "asg_desired_capacity" {
+  description = "Desired number of instances in Auto Scaling Group"
+  type        = number
+  default     = 3
+}
+
+# VPC & Subnet CIDRs
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDRs for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to deploy into"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 # S3 Variables
@@ -60,7 +105,6 @@ variable "db_name" {
 variable "db_class" {
   description = "DB instance class"
   type        = string
-  
 }
 
 variable "db_user" {
@@ -72,4 +116,10 @@ variable "db_pass" {
   description = "Password for DB"
   type        = string
   sensitive   = true
+}
+
+# Monitoring Variables
+variable "alert_email" {
+  description = "Email address to receive CloudWatch alerts"
+  type        = string
 }
